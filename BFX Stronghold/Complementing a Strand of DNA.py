3c76ff14ce416f1873e2348598236f13c79926cd
Dien "Ethan" Mach <ethanmach1998@gmail.com>
2023-01-04 14:16:24 -0800

    Added comments, implemented PEP-8 styling

--- BFX Stronghold/Complementing a Strand of DNA.py
+++ BFX Stronghold/Complementing a Strand of DNA.py
@@ -1,20 +1,35 @@
-dna_seq = 'TCTTCACTGTTACTCCTGTGAGATGGGTACCGAAGAAGTGAATCTGTCCGGTTCTCTGTATGGTCTATCTTCAATCACCATCCTAGTATCCCACATTCATAATTGGTGGGAAGTCTTGCAGACTACTCGGGCGTCGATTAAGGAACTGTACATGAGCAGATGAGGAATCATTGTGGCAAACGTCTCGCTGTGCGGACAGGGCAATATCAATAGGCCTCTTTTCCATCCCCTGACCCCTCCCGTCTTGCTCTCCACGCCACGTAGGTACCTGGGCAAAAAACATGCTTGCGTAAATAATTACCGTTAGACCTACTGGCCTCAAATGCGATGATAAGCCGAGTATGATGGCCAGATGACGATGAGATACTCGTTCGCCGAGAACTGCATACAGGTTGTGTTCAAGTGGCATGTCATTCATAGGCTCGTAAACATGCAATATTTGTTACGAACCGCATTGCTCGTTTAGCGTCGATCCACTAACCGGAGCGTAGTCTTATGACGGACTTGTCGTTATAGAATGCCTGTACTGAACCACCTAAACAGGTGGAATCACTTCCACAGCCATTATGCACTGCTGGATTTACCGAAAGCTACGGTGCAACCGTTCGGATTAGACGAGTACTGATCACGTAGTGCGGGATCGACGTACCGGTTCTTTGGGTGCTTTAGGCATGGCTCGGACCCTCCATTACGTCATACTTGGCTTTACCATGCCTGTCTACATATAGGACATACTTACGGGTACTACGAATTAGTTGGCCCCATCCGAGATGGAATACCAGAATCTCGCTATATTTAAGAACTGCACCATCGAGCCCCGAATGGCTTCATGGTTGTCAGGCCCATCACATGGTCATCC'
+def dna_complement(sequence: str):
+    """
+    This function takes an inputted DNA sequence and outputs the reverse complement version of the sequence
+    eg: a 5' -> 3' DNA sense strand is input, a 3' -> 5' DNA anti-sense strand is output
+    :param sequence: An inputted DNA sequence
+    :return: Reverse complement
+    """
+    # Initialize empty list
+    dna_list = []
 
-list = []
-for bp in dna_seq:
-    list.append(bp)
+    # Add each nucleotide into a list
+    for bp in sequence:
+        dna_list.append(bp)
 
-for i in range(len(list)):
-    if list[i] == 'A':
-        list[i] = 'T'
-    elif list[i] == 'C':
-        list[i] = 'G'
-    elif list[i] == 'T':
-        list[i] = 'A'
-    elif list[i] == 'G':
-        list[i] = 'C'
+    # Convert each nucleotide to complementing nucleotide
+    for i in range(len(dna_list)):
+        if dna_list[i] == 'A':
+            dna_list[i] = 'T'
+        elif dna_list[i] == 'C':
+            dna_list[i] = 'G'
+        elif dna_list[i] == 'T':
+            dna_list[i] = 'A'
+        elif dna_list[i] == 'G':
+            dna_list[i] = 'C'
 
-list.reverse()
-output = "".join(list)
+    # Reverse nucleotides in list, then convert list into string
+    dna_list.reverse()
+    output = "".join(dna_list)
+    return output
 
-print(output)
+
+if __name__ == '__main__':
+    print("What is the DNA sequence to be complemented?")
+    dna_seq = str(input())
+    print(dna_complement(dna_seq))
